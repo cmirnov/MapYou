f514fe3aedd49ccd9ba1174c5ef9245fdf68b494
cmirnov <cmirnov239kirill@gmail.com>
2017-04-04 22:35:04 +0300

    new version of MapFragment: add initial location, better codestyle

--- app/src/main/java/com/example/android/mapyou/MapFragment.java
+++ app/src/main/java/com/example/android/mapyou/MapFragment.java
@@ -1,14 +1,17 @@
 package com.example.android.mapyou;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.mapyou.R;
@@ -30,7 +33,9 @@ import com.google.android.gms.maps.model.PolygonOptions;
 
 import java.util.Vector;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+//import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static java.security.AccessController.getContext;
 
 
 public class MapFragment extends SupportMapFragment implements GoogleApiClient.ConnectionCallbacks,
@@ -40,61 +45,28 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     private GoogleApiClient mGoogleApiClient;
     private Location mCurrentLocation;
     private Communication mCommunication;
-    private LocationRequest mLocationRequest;
-    private LocationListener mListerner;
+    private Location mLastLocation;
 
-    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
-    private long FASTEST_INTERVAL = 2000; /* 2 sec */
-    private Integer count;
-
-    public  class DreamBeforeDrawing extends AsyncTask<Integer, Integer, Integer> {
-     //   private Integer myCount;
-
-        @Override
-        protected Integer doInBackground(Integer... params) {
-            //myCount = params[0];
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-
-            }
-            return params[0];
-        }
-
-        @Override
-        protected void onPostExecute(Integer ans) {
-           // Log.v("count", String.valueOf(count));
-            drawPolygon(ans);
-        }
-    }
-
+    private String FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
+    private String COARSE_LOCATION = "android.permission.ACCESS_COARSE_LOCATION";
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         setHasOptionsMenu(true);
+
         mCommunication = new Communication();
-        mListerner = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                mCurrentLocation = location;
-            }
-        };
-        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
 
         initListener();
-       // drawPolygon(new LatLng(60, 29.7657));
-       // drawPolygon(1);
-        count = 0;
-        Log.v("done", "done");
-        new DreamBeforeDrawing().execute(count, 1);
-       // f();
+        new DreamBeforeDrawing().execute(0, 0);
     }
+
 
     private void initListener() {
         getMap().setOnMarkerClickListener(this);
@@ -113,7 +85,7 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
     @Override
     public void onStop() {
         super.onStop();
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+        if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
     }
@@ -121,55 +93,45 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle bundle) {
-        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
-        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-       /* while (mCurrentLocation == null) {
-         //   LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
-            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        }*/
-       /* if (mCurrentLocation != null) {
-          //  mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-        } else {
-           // Toast.makeText(this, "kek", Toast.LENGTH_LONG).show();
 
-        }*/
+        while (getContext().checkCallingOrSelfPermission(FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)  {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
 
-       /* mCurrentLocation = LocationServices
+            }
+            Log.v("MapFragment", "LocationPermissionError");
+        }
+        mCurrentLocation = LocationServices
                 .FusedLocationApi
-                .getLastLocation(mGoogleApiClient);*/
+                .getLastLocation(mGoogleApiClient);
         initCamera(mCurrentLocation);
-        //f();
     }
 
-    private synchronized void drawPolygon (int color) { //location!
-        count += 10;
+    private synchronized void drawPolygon(LatLng point1, LatLng point2) {
         Vector<Building> buildings;
-        buildings = mCommunication.getBuildings(new LatLng(0, 0), new LatLng(0, 0), color); // zero
-        for (int i = 0; i < buildings.size(); ++i){
+        buildings = mCommunication.getBuildings(point1, point2, 0);
+        for (int i = 0; i < buildings.size(); ++i) {
             getMap().addPolygon(buildings.elementAt(i).getPolygon());
         }
-        new DreamBeforeDrawing().execute(count, 0);
-        //getMap().addPolygon( options );
+        new DreamBeforeDrawing().execute(0, 0);
     }
 
     private void initCamera(Location location) {
         CameraPosition position = CameraPosition.builder()
-                .target(new LatLng(59.880149, 29.825008))
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
                 .zoom(16f)
                 .bearing(0.0f)
                 .tilt(0.0f)
                 .build();
-        //new LatLng(location.getLatitude(), location.getLongitude())
         getMap().animateCamera(CameraUpdateFactory.newCameraPosition(position));
-
         getMap().setMyLocationEnabled(true);
         getMap().getUiSettings().setZoomControlsEnabled(true);
     }
 
-   @Override
+    @Override
     public void onConnectionSuspended(int i) {
-
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -189,23 +151,13 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-
-        Log.v("Long", String.valueOf(lastLocation.getLongitude()));
-        Log.v("Lat", String.valueOf(lastLocation.getLatitude()));
 
     }
 
-    protected void startLocationUpdates() {
-        // Create the location request
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(UPDATE_INTERVAL)
-                .setFastestInterval(FASTEST_INTERVAL);
-        // Request location updates
-       // LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
-         //       mLocationRequest, this);
+    private void upDateLocation() {
+        mCurrentLocation = LocationServices
+                .FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
     }
 
     @Override
@@ -213,5 +165,23 @@ public class MapFragment extends SupportMapFragment implements GoogleApiClient.C
         return false;
     }
 
+    public class DreamBeforeDrawing extends AsyncTask<Integer, Integer, Integer> {
 
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            try {
+                Thread.sleep(5000);
+
+            } catch (InterruptedException e) {
+
+            }
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(Integer ans) {
+            upDateLocation();
+            drawPolygon(new LatLng(59.808263, 29.536778), new LatLng(60.109892, 30.723302)); //Peterhof: (59.870843, 29.765774) (59.887411, 29.883190)
+        }
+    }
 }
